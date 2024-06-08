fbfc9c9f67a562d2be1f1759eb184042083ac22b
testheader <vdlgeert@gmail.com>
2024-06-08 13:13:48 +0200

    First attempt at tests!

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
+import React from "react";
+import {BrowserRouter} from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('verify default components are present', () => {
+  render(<BrowserRouter><App /></BrowserRouter>)
+  const homeComponent = screen.getByTestId("homeComponent")
+  const footerComponent = screen.getByTestId("footerComponent")
+
+  expect(homeComponent).toBeInTheDocument();
+  expect(footerComponent).toBeInTheDocument();
 });
+
+test('overview button is hidden then shown', async () => {
+  render(<BrowserRouter><App/></BrowserRouter>)
+  let overviewButton = screen.queryByTestId("overviewButton")
+  expect(overviewButton).not.toBeInTheDocument();
+
+  let showOverview = screen.getByTestId("showOverview")
+  await fireEvent.click(showOverview)
+
+  overviewButton = screen.queryByTestId("overviewButton")
+  expect(overviewButton).toBeInTheDocument();
+});
+
